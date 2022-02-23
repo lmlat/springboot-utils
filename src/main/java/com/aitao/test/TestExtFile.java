3c7bed7d24283f6124faa9baa103d89aa679ee13
aitao <aitao@qianxin.com>
2022-02-23 18:26:28 +0800

    init project

--- src/main/java/com/aitao/test/TestExtFile.java
+++ src/main/java/com/aitao/test/TestExtFile.java
@@ -11,6 +11,6 @@ public class TestExtFile {
     public static void main(String[] args) {
         ExtFile file = new ExtFile("C:\\Users\\10034\\Desktop\\Learn\\springboot-utils\\src\\main\\resources\\static\\application.properties");
         System.out.println(file.getType());
-        System.out.println(file.getFileType());
+        System.out.println(file.getMimeType());
     }
 }
